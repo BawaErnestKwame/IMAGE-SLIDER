880be5c17d6338e36cd15c256b3df6615f207e29
BawaErnestKwame <bawaernest926@gmail.com>
2024-10-04 10:33:43 +0000

    complete

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const 
+let scrollbarContainer = document.querySelector(".gallery");
+
+let arrowNext = document.getElementById("arrow-next");
+let arrowBack = document.getElementById("arrow-back");
+
+scrollbarContainer.addEventListener("wheel", (e) => {
+    scrollbarContainer.scrollLeft += e.deltaY;
+    scrollbarContainer.style.scrollBehavior = "auto";  
+});
+
+arrowNext.addEventListener("click", () => {
+    scrollbarContainer.style.scrollBehavior = "smooth";  
+    scrollbarContainer.scrollLeft += 900;  
+});
+
+arrowBack.addEventListener("click", () => {
+    scrollbarContainer.style.scrollBehavior = "smooth";  
+    scrollbarContainer.scrollLeft -= 900; 
+});
+
+
+
